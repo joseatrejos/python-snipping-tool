c2251ff97ffdc83584d4e96afdfb13ebdce73a99
José Trejo <joseatrejos@gmail.com>
2023-06-02 02:06:40 -0700

    Final optimization research

--- image_recognition.py
+++ image_recognition.py
@@ -16,6 +16,14 @@ def find_images_on_screen(images):
         found_image = False
 
         for index, target_image in enumerate(target_images):
+            
+            # # How to define a search region for optimization purposes
+            # # If you want to define a search region starting from (100, 100) and extending 200 pixels 
+            # # to the right and 150 pixels down, you would set search_region as (100, 100, 200, 150). 
+            # # This creates a rectangular region of 200 pixels in width and 150 pixels in height, 
+            # # starting from 100 pixels of distance from the top and from the left of your screen's
+            # # top-left corner.
+            # search_region = (100, 100, 800, 600)  # Adjust the coordinates as per your requirement
 
             # Search for the target image on the screen
             location = pyautogui.locateOnScreen(target_image)
@@ -23,7 +31,7 @@ def find_images_on_screen(images):
             # If image is found
             if location is not None:
                 found_image = True
-                print(f"Image {index} found at location:", location)
+                print(f"Image {target_image.filename.split('/')[1]} found at location:", location)
 
         # Check if no images have been found
         if found_image == False:
