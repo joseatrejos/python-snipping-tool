ba4af57ffc9a9196d6a4993c7c8b6948420a3cfa
Orlando Camacho Gámez <92180857+orlandocamacho19@users.noreply.github.com>
2023-06-21 12:46:48 -0700

    Error handler

--- utils/export_script.py
+++ utils/export_script.py
@@ -14,9 +14,11 @@ def export_script(script, file_name):
         # if file_name already exist add a number
         python_script = open(f'scripts/{file_name}{i}.py', 'w')
         file_path = f'scripts/{file_name}{i}.py'
+        file_name = f'{file_name}{i}.py'
     else:
         python_script = open(f'scripts/{file_name}.py', 'w')
         file_path = f'scripts/{file_name}.py'
+        file_name = f'{file_name}.py'
 
     python_script.write("import time\n")
     python_script.write("import pyautogui, os\n")
@@ -32,17 +34,29 @@ def export_script(script, file_name):
     if "Get API" in steps[0]:
         url = steps[0].split('|')[4]
         python_script.write(f"    json_data = request_get(\'{url}\')\n\n")
+        python_script.write(f"    if not json_data:\n")
+        python_script.write(f"        return True\n\n")
         steps.pop(0)
 
         python_script.write("    for object in json_data:\n\n")
         
         for i, step in enumerate(steps):
-            python_script.write(f"        # Start step #{i}\n")
+            python_script.write(f"        # Start step #{i+1}\n")
             
             image_x_y_action = step.split('|')
 
             python_script.write("        image_found = False\n")
+            python_script.write("        time_iteration = time.time()\n")
             python_script.write("        while not image_found:\n")
+            python_script.write("            actual_time = time.time()\n")
+            python_script.write("            if actual_time - time_iteration >= 15:\n")
+            python_script.write("                payload = {}\n")
+            python_script.write("                files = {}\n")
+            python_script.write("                headers = {\"Content-Type\": \"application/json\"}\n")
+            python_script.write("                url = 'https://discord.com/api/webhooks/1120868077882593291/MsA5EBTCQy1yvViUbAttF997VuEyRN0FRUBoN_BiN4Owe6HA0P7plCII9cxV57x-jrV_'\n")
+            python_script.write("                webhook_data = {'url': url, 'files': files, 'headers': headers}\n")
+            python_script.write(f"                send_message_to_webhook(webhook_data, f\"**Image not found**\", \"{file_name} error at step #{i+1}\", \"{step}\", 0xFF0000)\n")
+            python_script.write("                return True\n\n")
             python_script.write(f"            image = Image.open(\'./{image_x_y_action[0]}\')\n")
             python_script.write("            location = pyautogui.locateOnScreen(image)\n")
             python_script.write("            if location is not None:\n")
@@ -70,17 +84,27 @@ def export_script(script, file_name):
                 python_script.write("                pyautogui.typewrite(str(value))\n")
                 python_script.write("                pyautogui.press('enter')\n")
 
-            python_script.write("            image_found = True\n")
-            python_script.write(f"        # End step #{i}\n\n")
+            python_script.write("                image_found = True\n")
+            python_script.write(f"        # End step #{i+1}\n\n")
 
     else:
         for i, step in enumerate(steps):
-            python_script.write(f"    # Start step #{i}\n")
+            python_script.write(f"    # Start step #{i+1}\n")
             
             image_x_y_action = step.split('|')
 
             python_script.write("    image_found = False\n")
+            python_script.write("    time_iteration = time.time()\n")
             python_script.write("    while not image_found:\n")
+            python_script.write("        actual_time = time.time()\n")
+            python_script.write("        if actual_time - time_iteration >= 15:\n")
+            python_script.write("            payload = {}\n")
+            python_script.write("            files = {}\n")
+            python_script.write("            headers = {\"Content-Type\": \"application/json\"}\n")
+            python_script.write("            url = 'https://discord.com/api/webhooks/1120868077882593291/MsA5EBTCQy1yvViUbAttF997VuEyRN0FRUBoN_BiN4Owe6HA0P7plCII9cxV57x-jrV_'\n")
+            python_script.write("            webhook_data = {'url': url, 'files': files, 'headers': headers}\n")
+            python_script.write(f"            send_message_to_webhook(webhook_data, f\"**Image not found**\", \"{file_name} error at step #{i+1}\", \"{step}\", 0xFF0000)\n")
+            python_script.write("            return True\n\n")
             python_script.write(f"        image = Image.open(\'./{image_x_y_action[0]}\')\n")
             python_script.write("        location = pyautogui.locateOnScreen(image)\n")
             python_script.write("        if location is not None:\n")
@@ -108,9 +132,8 @@ def export_script(script, file_name):
                 python_script.write("            pyautogui.typewrite(str(value))\n")
                 python_script.write("            pyautogui.press(\'enter\')\n")
 
-            python_script.write("        image_found = True\n")
-            python_script.write(f"    # End step #{i}\n\n")
-
+            python_script.write("            image_found = True\n")
+            python_script.write(f"    # End step #{i+1}\n\n")
 
     python_script.write("def request_get(url):\n")
     python_script.write("    try:\n")
@@ -119,7 +142,13 @@ def export_script(script, file_name):
     python_script.write("        json_data = response.json()\n")
     python_script.write("        return json_data\n")
     python_script.write("    except requests.exceptions.RequestException as e:\n")
-    python_script.write("        print(\"Error\", e)\n\n")
+    python_script.write("        payload = {}\n")
+    python_script.write("        files = {}\n")
+    python_script.write("        headers = {\"Content-Type\": \"application/json\"}\n")
+    python_script.write("        url = 'https://discord.com/api/webhooks/1120868077882593291/MsA5EBTCQy1yvViUbAttF997VuEyRN0FRUBoN_BiN4Owe6HA0P7plCII9cxV57x-jrV_'\n")
+    python_script.write("        webhook_data = {'url': url, 'files': files, 'headers': headers}\n")
+    python_script.write("        send_message_to_webhook(webhook_data, f\"**Fail request (get)**\", \"Excepted Json\", f\"{e}\", 0xFF0000)\n\n")
+    python_script.write("        return False\n")
 
     python_script.write("def find_key_value(json_data, json_keys):\n")
     python_script.write("    keys = json_keys.split(\'-\')\n")
@@ -128,6 +157,19 @@ def export_script(script, file_name):
     python_script.write("            break\n")
     python_script.write("        json_data = json_data[key]\n")
     python_script.write("    return json_data\n\n")
+
+    python_script.write("def send_message_to_webhook(webhook_data, content, embed_title, embed_description, embed_color=0x00FF00):\n")
+    python_script.write("    payload={\n")
+    python_script.write("        \"content\": content,\n")
+    python_script.write("        \"embeds\": [\n")
+    python_script.write("            {\n")
+    python_script.write("                \"title\": embed_title,\n")
+    python_script.write("                \"description\": embed_description,\n")
+    python_script.write("                \"color\": embed_color\n")
+    python_script.write("            }\n")
+    python_script.write("        ]\n")
+    python_script.write("    }\n\n")
+    python_script.write("    requests.request(\"POST\", webhook_data[\'url\'], headers=webhook_data[\'headers\'], data=json.dumps(payload), files=webhook_data[\'files\'])\n\n")
 
     python_script.write("if __name__ == \'__main__\':\n")
     python_script.write("    run_script()\n\n")
@@ -140,7 +182,8 @@ def export_script(script, file_name):
     python_script.write("#             stop_iteration = True\n\n")
     python_script.write("#     keyboard.on_press(stop_infinite_loop)\n\n")
     python_script.write("#     while not stop_iteration:\n")
-    python_script.write("#         run_script()\n\n")
+    python_script.write("#         if run_script() == True:\n")
+    python_script.write("#             stop_iteration = True\n")
     python_script.write("#     keyboard.unhook_all()\n")
     python_script.close()
 
